9bbb5f9804732941d9ab33f9a7ff6b675fb34f1a
lsindhu <sindhu151095@gmail.com>
2024-09-18 09:50:04 +0530

    Intro/useState hook

---
+++ 01-tut/src/Content.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useState } from 'react'
+
+const Content = () => {
+    const [items, setItems] = useState('Sindhu')
+
+    
+
+  return (
+    <main>
+        
+    </main>
+  )
+}
+
+export default Content
